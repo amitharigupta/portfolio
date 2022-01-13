b7e551f8c2765bbe679c188dedc7165a6975d803
amitharigupta <amitgt9967@gmail.com>
2022-01-13 21:45:21 +0530

    initial commit

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ContactSection from '../components/ContactSection';
+import Map from '../components/Map';
+
+const Contact = () => {
+  return (
+    <>
+      <ContactSection />
+      <Map />
+    </>
+  )
+}
+
+export default Contact
